45f1bf2e6337206c4a9d20cbb317571229f00c9d
Kperp8 <kacper.pustulka@interia.com>
2026-06-16 01:03:58 +0200

    encja procedur

--- Domain/Procedures.cs
+++ Domain/Procedures.cs
@@ -1,4 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Procedures
 {
-    public int Id {get; set;}
+    public int Id { get; set; }
+
+    [StringLength(350, ErrorMessage = "Opis może mieć maksymalnie 350 znaków")]
+    public string Description { get; set; } = null!;
+
+    public Decimal Price { get; set; }
 }
---
+++ Domain/Procedures/IProceduresRepository.cs
@@ -0,0 +1,13 @@
+namespace Domain.Procedure;
+
+public interface IProceduresRepository
+{
+    Task<Procedures?> GetByIdAsync(int id);
+    Task<IReadOnlyList<Procedures>> GetPagedAsync(int page, int pageSize);
+    Task<IReadOnlyList<Procedures>> SearchAsync(string query);
+    Task<IReadOnlyList<Procedures>> GetByPrice(Decimal price);
+    Task AddAsync(Procedures procedure);
+    Task UpdateAsync(Procedures procedure);
+    Task DeleteAsync(Procedures procedure);
+    Task SaveChangesAsync();
+}
