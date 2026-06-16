4180fec4c62b0c9f23aa205650ed2741f30a6378
Kperp8 <kacper.pustulka@interia.com>
2026-06-16 20:31:39 +0200

    razor pages dla lekow

--- Domain/Medications/IMedicationsRepository.cs
+++ Domain/Medications/IMedicationsRepository.cs
@@ -5,7 +5,7 @@ public interface IMedicationsRepository
     Task<Medication?> GetByIdAsync(int id);
     Task<IReadOnlyList<Medication>> GetPagedAsync(int page, int pageSize);
     Task<IReadOnlyList<Medication>> SearchAsync(string query);
-    Task<IReadOnlyList<Medication>> GetByNameAsync(string bloodType);
+    Task<IReadOnlyList<Medication>> GetByNameAsync(string name);
     Task<IReadOnlyList<Medication>> GetByCostAsync(Decimal cost);
     Task AddAsync(Medication medication);
     Task UpdateAsync(Medication medication);
