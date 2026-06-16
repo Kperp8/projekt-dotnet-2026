eb98d9ad7d78cb37160587fb29dc6e9b9d123078
Kperp8 <kacper.pustulka@interia.com>
2026-06-16 13:02:32 +0200

    encja lekow

---
+++ Domain/Medications/IMedicationsRepository.cs
@@ -0,0 +1,14 @@
+namespace Domain.Medications;
+
+public interface IMedicationsRepository
+{
+    Task<Medication?> GetByIdAsync(int id);
+    Task<IReadOnlyList<Medication>> GetPagedAsync(int page, int pageSize);
+    Task<IReadOnlyList<Medication>> SearchAsync(string query);
+    Task<IReadOnlyList<Medication>> GetByNameAsync(string bloodType);
+    Task<IReadOnlyList<Medication>> GetByCostAsync(Decimal cost);
+    Task AddAsync(Medication medication);
+    Task UpdateAsync(Medication medication);
+    Task DeleteAsync(Medication medication);
+    Task SaveChangesAsync();
+}
